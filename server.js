4c4092a98f334c0af62eaa76fba026ac6370f01c
Adrian Tili <adrian.tili@travelport.com>
2019-11-28 15:17:16 +0200

    Database connection

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/database");
 
 const app = express();
+
+//Connect to Database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
